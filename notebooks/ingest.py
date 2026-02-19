c417a3817c2a0a9fe0bb3061bb7ba8115d9734aa
Prtk22 <perfectpratikdev@gmail.com>
2026-02-19 17:09:53 +0530

    Ingestion statement changed

--- notebooks/ingest.py
+++ notebooks/ingest.py
@@ -3,4 +3,4 @@ from pyspark.sql.functions import rand
 df = spark.range(100).withColumn("amount", (rand()*1000).cast("int"))
 df.write.mode("overwrite").saveAsTable("workspace.bronze.demo_raw_sales")
 
-print("Ingestion is Complete")
+print("Ingestion done")
